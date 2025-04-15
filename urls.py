c137c4ab5238428ea9170363e88425cb0cfb17f1
Kamronbek <115008454+Kamronbek477@users.noreply.github.com>
2025-04-15 09:37:38 +0500

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('products/', include('products.urls')),
+    path('', views.product_list, name='product_list'),
 ]
